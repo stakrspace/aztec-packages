86684f2de75ee04ddb775c773b5178a2f9a75a7b
IlyasRidhuan <ilyasridhuan@gmail.com>
2025-11-05 16:49:50 +0000

    feat(avm): basic bitwise fuzzer

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/harness/alu.fuzzer.cpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/harness/alu.fuzzer.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <fuzzer/FuzzedDataProvider.h>
 
+#include "barretenberg/avm_fuzzer/harness/mutation_helper.hpp"
 #include "barretenberg/common/serialize.hpp"
 #include "barretenberg/numeric/uint256/uint256.hpp"
 #include "barretenberg/vm2/common/field.hpp"
@@ -29,58 +30,13 @@
 using namespace bb::avm2::simulation;
 using namespace bb::avm2::tracegen;
 using namespace bb::avm2::constraining;
+using namespace bb::avm2::fuzzing;
 
 using bb::avm2::FF;
 using bb::avm2::MemoryTag;
 using bb::avm2::MemoryValue;
 
 using alu_rel = bb::avm2::alu<FF>;
-
-namespace {
-
-// Do we want the option of making "invalid tag" values, where the value is out of range for the tag?
-// These aren't currently possible with this function since MemoryValue::from_tag will throw in that case.
-MemoryValue read_mem_value(FuzzedDataProvider& fdp)
-{
-    // Grab 32 bytes for a uint256
-    uint64_t limb0 = fdp.ConsumeIntegral<uint64_t>();
-    uint64_t limb1 = fdp.ConsumeIntegral<uint64_t>();
-    uint64_t limb2 = fdp.ConsumeIntegral<uint64_t>();
-    uint64_t limb3 = fdp.ConsumeIntegral<uint64_t>();
-
-    uint256_t value = uint256_t(limb0, limb1, limb2, limb3);
-
-    int tag_choice = fdp.ConsumeIntegralInRange<int>(0, 6);
-    switch (tag_choice) {
-    case 0:
-        return MemoryValue::from_tag_truncating(MemoryTag::U1, FF(value));
-        break;
-    case 1:
-        return MemoryValue::from_tag_truncating(MemoryTag::U8, FF(value));
-        break;
-    case 2:
-        return MemoryValue::from_tag_truncating(MemoryTag::U16, FF(value));
-        break;
-    case 3:
-        return MemoryValue::from_tag_truncating(MemoryTag::U32, FF(value));
-        break;
-    case 4:
-        return MemoryValue::from_tag_truncating(MemoryTag::U64, FF(value));
-        break;
-    case 5:
-        return MemoryValue::from_tag_truncating(MemoryTag::U128, FF(value));
-        break;
-    case 6:
-        return MemoryValue::from_tag_truncating(MemoryTag::FF, FF(value));
-        break;
-    default:
-        assert(false && "unreachable");
-    }
-    // To statisfy compiler
-    return MemoryValue::from_tag_truncating(MemoryTag::FF, FF(0));
-}
-
-} // namespace
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
