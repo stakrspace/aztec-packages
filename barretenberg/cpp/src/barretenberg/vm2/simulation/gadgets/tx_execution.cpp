a2574dd25290a81806e56e5e81a700bddb93d13f
jeanmon <jean@aztec-labs.com>
2025-11-14 08:49:04 +0000

    Fix completeness issue related to reading gas limit and pi offset

--- barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/tx_execution.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/tx_execution.cpp
@@ -67,7 +67,7 @@ TxExecutionResult TxExecution::simulate(const Tx& tx)
     std::optional<std::vector<FF>> app_logic_return_value;
 
     events.emit(TxStartupEvent{
-        .state = tx_context.serialize_tx_context_event(),
+        .gas_used = tx_context.gas_used,
         .gas_limit = gas_limit,
         .teardown_gas_limit = teardown_gas_limit,
         .phase_lengths = PhaseLengths::from_tx(tx), // extract lengths of each phase at start
